6c020f84f79e7bab87d4a0e4c9edf71163803609
Pradeep Kumar Maurya <88256058+pradeep4j@users.noreply.github.com>
2024-07-31 22:50:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
 app.get('/',(req,res) => {
-    res.json({message:'Backend has been run successfully!'})
+    res.json({message:'Backend has been run successfully!-testing with new build jinkins pipeline!'})
 })
 app.listen(3000);

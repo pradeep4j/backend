5a7be1e645b63f353294c8c2e710c279d078812d
Pradeep Kumar Maurya <88256058+pradeep4j@users.noreply.github.com>
2024-07-31 21:57:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
 app.get('/',(req,res) => {
-    res.json({status:200,message:'backend'})
+    res.json({message:'Backend is run successfully!'})
 })
 app.listen(3000);
